4ef61c91a91d039b0ba7d6718857711769004e7e
susu <sjy8593457@outlook.com>
2025-03-07 23:02:11 +0800

    feat: automation daily update

---
+++ auto_push.ps1
@@ -0,0 +1,34 @@
+# 自动提交本项目
+function Push-Auto
+{
+    param (
+        [int] $time = 0
+    )
+    try
+    {
+        git pull origin master;
+        git add .;
+        git commit -m "feat: automation daily update";
+        git push -u origin master;
+        git push -u gitub master;
+        if ($time -eq 0)
+        {
+            Write-Host "push success, Waiting for seconds to clear the terminal..." -ForegroundColor Green
+            Start-Sleep -s 4;
+            Clear-Host
+        }
+        else
+        {
+            Write-Host "push success, Waiting for $time seconds to clear the terminal..." -ForegroundColor Green
+            Start-Sleep -s $time;
+            Clear-Host
+        }
+    }
+    catch
+    {
+        Write-Host "push failed, please check your network or git config" -ForegroundColor Red
+    }
+}
+
+
+Push-Auto -time 3
